4b55e4e2116e428e681b745ff955792864dc7461
Lukas Läderach <lukas.laederach@3volutions.ch>
2017-05-19 11:04:33 +0200

    log every third interrupt to serial out

---
+++ arduino/lichtschranke/lichtschranke.ino
@@ -0,0 +1,42 @@
+
+// constants won't change. Used here to set a pin number :
+const int irLedPin =  2;
+const int photoTranseDigitalIn = 3;
+
+int interuptCounter = 0;
+int irState = 0;
+int lastIrState = 0;
+int states[500];
+int i = 0;
+
+void setup() {
+  // set the digital pin as output:
+  Serial.begin(9600);
+  pinMode(irLedPin, OUTPUT);
+  pinMode(photoTranseDigitalIn, INPUT);
+  digitalWrite(irLedPin, HIGH);
+  delay(50);
+}
+
+void loop() {
+  int currentIrState = digitalRead(photoTranseDigitalIn);
+  
+  if (currentIrState != lastIrState) {
+    if (currentIrState == HIGH) {
+      interuptCounter++;
+      Serial.print("number of interupts: ");
+      Serial.println(interuptCounter);
+    }
+  }
+
+  lastIrState = currentIrState;
+  
+   if (interuptCounter > 0 && interuptCounter % 3 == 0) {
+    Serial.println("");
+    Serial.println("********************");
+    Serial.println("** 3x interrupted **");
+    Serial.println("********************");
+    interuptCounter = 0;
+  }
+}
+
