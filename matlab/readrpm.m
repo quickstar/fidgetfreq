40d0038003788f12da3fe0265cce859e6653b7fb
Lukas Läderach <lukas.laederach@3volutions.ch>
2017-05-19 13:03:35 +0200

    try to visualize the received data

--- matlab/readrpm.m
+++ matlab/readrpm.m
@@ -3,7 +3,12 @@ s1 = serial('COM4');                            %define serial port
 s1.BaudRate = 9600;                               %define baud rate
 
 fopen(s1);
-    data=fscanf(s1);
-    vector = strsplit(data, ':');
-    plot(vector(1), vector(2));
+figure
+    while(1)
+        data=fscanf(s1);
+        vector = strsplit(data, ':');
+        x = str2double(vector(1));
+        y = str2double(vector(2));
+        plot(x, y);
+    end
 fclose(s1);
