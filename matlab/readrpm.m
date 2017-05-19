fa7347ac75e3510e8e08e80317f2d3961acf29f3
Lukas Läderach <lukas.laederach@3volutions.ch>
2017-05-19 13:07:30 +0200

    try reading data until we reach a timeout (no more data is sent)

--- matlab/readrpm.m
+++ matlab/readrpm.m
@@ -1,11 +1,16 @@
 clear all;
-s1 = serial('COM4');                            %define serial port
-s1.BaudRate = 9600;                               %define baud rate
+s1 = serial('COM4');    %define serial port
+s1.BaudRate = 9600;     %define baud rate
 
 fopen(s1);
 figure
     while(1)
-        data=fscanf(s1);
+        try
+            data=fscanf(s1);
+        catch err
+            break;
+        end
+
         vector = strsplit(data, ':');
         x = str2double(vector(1));
         y = str2double(vector(2));
